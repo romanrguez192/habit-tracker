65e105ce989449cb02752ee8c8d342081fc1d4fa
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-03-24 19:52:02 -0400

    Tablas listas
    Creadas setbacks y reviews

--- db_connection.py
+++ db_connection.py
@@ -9,6 +9,8 @@ try:
     cur.execute("CREATE TABLE IF NOT EXISTS cycles(cycle_id INTEGER NOT NULL PRIMARY KEY, initial_date TEXT NOT NULL);")
     cur.execute("CREATE TABLE IF NOT EXISTS habits(habit_id INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL, action TEXT NOT NULL, measurement TEXT NOT NULL, days TEXT NOT NULL, cycle_id INTEGER NOT NULL, FOREIGN KEY(cycle_id) REFERENCES cycles(cycle_id) ON DELETE CASCADE);")
     cur.execute("CREATE TABLE IF NOT EXISTS tracking(habit_id INTEGER NOT NULL, day INTEGER NOT NULL, status TEXT NOT NULL, PRIMARY KEY(habit_id, day), FOREIGN KEY(habit_id) REFERENCES habits(habit_id) ON DELETE CASCADE);")
+    cur.execute("CREATE TABLE IF NOT EXISTS setbacks(setback_id INTEGER NOT NULL PRIMARY KEY, day INTEGER NOT NULL, description TEXT NOT NULL, cycle_id INTEGER NOT NULL, FOREIGN KEY(cycle_id) REFERENCES cycles(cycle_id) ON DELETE CASCADE);")
+    cur.execute("CREATE TABLE IF NOT EXISTS reviews(review_id INTEGER NOT NULL PRIMARY KEY, description TEXT NOT NULL, cycle_id INTEGER NOT NULL, FOREIGN KEY(cycle_id) REFERENCES cycles(cycle_id) ON DELETE CASCADE);")
 
     con.commit()
 
