029624306069f9e5c33242433a925da889692daa
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-03-24 13:45:37 -0400

    Menú de opciones
    No funcional

--- habit_tracker.py
+++ habit_tracker.py
@@ -4,20 +4,34 @@ LANGUAGES = messages.keys()
 lang = ""
 
 try:
+    # Intenta abrir el archivo
     with open("lang.txt", "r") as f:
         lang = f.read(2).upper()
+        # Si no es un idioma válido
         if lang not in LANGUAGES:
             raise FileNotFoundError
 
+# En caso de error, se solicita el idioma
 except FileNotFoundError:
     while lang not in LANGUAGES:
         for lang_messages in messages.values():
             print(lang_messages["choose"])
         lang = input(">>> ").upper()
     
+    # Escribe en el archivo
     with open("lang.txt", "w") as f:
         f.write(lang)
 
+# Bienvenida a la aplicación
 messages = messages[lang]
 print(messages["welcome"])
+
+# Menú de opciones
+option = ""
+while option not in map(str, [i + 1 for i in range(3)]):
+    print(messages["current_cycle"])
+    print(messages["past_cycles"])
+    print(messages["exit"])
+    option = input(">>> ")
+
 
--- messages.py
+++ messages.py
@@ -1,10 +1,16 @@
 messages = {
     "EN": {
         "choose": "What language do you speak? (EN)",
-        "welcome": "Welcome!"
+        "welcome": "Welcome!",
+        "current_cycle": "1. Current cycle",
+        "past_cycles": "2. Past cycles",
+        "exit": "3. Exit",
     },
     "ES": {
         "choose": "¿Qué idioma hablas? (ES)",
-        "welcome": "¡Bienvenido!"
+        "welcome": "¡Bienvenido!",
+        "current_cycle": "1. Ciclo actual",
+        "past_cycles": "2. Ciclos anteriores",
+        "exit": "3. Salir",
     }
 }
